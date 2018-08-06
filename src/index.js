e62c81dadc9fbc20912d90cfd17cd3aa172b39a9
Lauren Perchitti <laurenperchitti@gmail.com>
2018-08-06 20:31:48 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import shoppingListItemReducer from './reducers/shoppingListItemReducer';
-import App from './App';
-import './index.css';
+import App from './App'
+
+import manageUsers from './reducers/manageUsers'
+
 
 const store = createStore(
-  shoppingListItemReducer,
+  manageUsers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+  <App store={store} />
   </Provider>,
   document.getElementById('root')
 );
